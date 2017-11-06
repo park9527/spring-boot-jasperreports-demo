06e73ccf41f613db94a8e209c6ca1dc0aed36db6
lujun.chen <lujun.chen@kuanrf.com>
2017-11-06 23:13:09 +0800

    封装数据

--- src/main/java/com/itclj/common/JasperConfig.java
+++ src/main/java/com/itclj/common/JasperConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.jasperreports.JasperReportsViewResol
 public class JasperConfig extends WebMvcConfigurerAdapter {
 
     private final static String REPORT_DATA_KEY = "datasource";
-    private final static String PATH_KEY = "classpath:jaspertemplate/";
+    private final static String PATH_KEY = "classpath:jrxml/";
     private final static String TYPE_KEY = ".jrxml";
     private final static String VIEW_KEY = "Report";
 
